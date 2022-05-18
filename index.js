d39996864253874a977a0ae735dd8b77d3840743
DFeliciano3 <91075867+DFeliciano3@users.noreply.github.com>
2022-05-18 09:15:31 -0400

    commit tree 040e04500bebbb141dcb5d5acb1e0e836af99813

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
 
 // Write your code here
+let num1 = 2;
+let num2 = 31;
+const multiply = num1 * num2;
+
+
+const random = Math.floor(Math.random(2) * 10) + 1
+
+let num3 = 32
+let num4 = 8
+const mod = num3 / num4
+
+const max = Math.max(5,10,15,20)
